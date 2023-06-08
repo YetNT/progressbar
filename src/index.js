474d0371c4719dac80ff9f6c7b7484a4ad5c5df6
Yetity <hlonipoole692@gmail.com>
2023-06-08 18:26:20 +0200

    prettier

--- src/index.js
+++ src/index.js
@@ -6,67 +6,107 @@
  * @param {Boolean} returnAr Return an array in stead of a string
  * @param {String[]} firstEdgeOverride Override first bar char with something else. Make sure it is similar to this ```["emptyChar", "fullChar"]```.
  * @param {String[]} lastEdgeOverride Override first bar char with something else. Make sure it is similar to this ```["emptyChar", "fullChar"]```.
- * 
+ *
  * @returns Array or String when `returnAr` is specified
  */
-const progressBar = (percentage, barWidth, emptyChars, fullChars, returnAr = false, firstEdgeOverride, lastEdgeOverride) => { // (Percent of the bar you want full (out of 100), Amount of units in the bar)
-// checks
-  if (percentage > 100 || percentage < 0 || typeof percentage !== 'number' || !Number.isInteger(percentage) ) {
-    console.error(`percentage should be a valid integer/float between 0 and 100.`)
-    let out = ``
-    percentage > 100 ? out = `${percentage} > 100` : percentage < 0 ? out = `${percentage} < 0` : out = `percentage is currently a ${typeof percentage}`
-    console.error(out)
-    return;
-  }
+const progressBar = (
+    percentage,
+    barWidth,
+    emptyChars,
+    fullChars,
+    returnAr = false,
+    firstEdgeOverride,
+    lastEdgeOverride
+) => {
+    // (Percent of the bar you want full (out of 100), Amount of units in the bar)
+    // checks
+    if (
+        percentage > 100 ||
+        percentage < 0 ||
+        typeof percentage !== "number" ||
+        !Number.isInteger(percentage)
+    ) {
+        console.error(
+            `percentage should be a valid integer/float between 0 and 100.`
+        );
+        let out = ``;
+        percentage > 100
+            ? (out = `${percentage} > 100`)
+            : percentage < 0
+            ? (out = `${percentage} < 0`)
+            : (out = `percentage is currently a ${typeof percentage}`);
+        console.error(out);
+        return;
+    }
 
-  if (typeof barWidth !== 'number') {
-    console.error(`Barwidth should be a valid Int`)
-    return
-  }
+    if (typeof barWidth !== "number") {
+        console.error(`Barwidth should be a valid Int`);
+        return;
+    }
 
-  if (typeof fullChars !== 'string' || typeof emptyChars !== 'string') {
-    console.error(`emptyChar (param 3) and fullChar (param 4) should be of type String`)
-    return
-  }
+    if (typeof fullChars !== "string" || typeof emptyChars !== "string") {
+        console.error(
+            `emptyChar (param 3) and fullChar (param 4) should be of type String`
+        );
+        return;
+    }
 
-  if ((firstEdgeOverride !== undefined) && (firstEdgeOverride.length !== lastEdgeOverride.length)) {
-    console.error(`Overrides should only be 2 elements long.`)
-    return;
-  }
-// logic
-  var units = Math.floor((percentage / 100) * barWidth) // Amount of units that will be shaded (Do Not Edit)
-  var bar = []
-  let empty = "", filled = "";
-  
-  for ( i = 0; i < barWidth; i++) {
+    if (
+        firstEdgeOverride !== undefined &&
+        firstEdgeOverride.length !== lastEdgeOverride.length
+    ) {
+        console.error(`Overrides should only be 2 elements long.`);
+        return;
+    }
+    // logic
+    var units = Math.floor((percentage / 100) * barWidth); // Amount of units that will be shaded (Do Not Edit)
+    var bar = [];
+    let empty = "",
+        filled = "";
 
-    if (emptyChars == undefined && fullChars == undefined) {
-      empty = "empty", filled = "filled"
+    for (i = 0; i < barWidth; i++) {
+        if (emptyChars == undefined && fullChars == undefined) {
+            (empty = "empty"), (filled = "filled");
+        } else {
+            (empty = emptyChars), (filled = fullChars);
+        }
+
+        if (i < units) {
+            // push the filled char
+            if (
+                typeof firstEdgeOverride !== "undefined" &&
+                typeof lastEdgeOverride !== "undefined"
+            ) {
+                i == 0
+                    ? bar.push(firstEdgeOverride[1])
+                    : i == barWidth - 1
+                    ? bar.push(lastEdgeOverride[1])
+                    : bar.push(filled);
+            } else {
+                bar.push(filled);
+            }
+        } else {
+            // push the empty char
+            if (
+                typeof firstEdgeOverride !== "undefined" &&
+                typeof lastEdgeOverride !== "undefined"
+            ) {
+                i == 0
+                    ? bar.push(firstEdgeOverride[0])
+                    : i == barWidth - 1
+                    ? bar.push(lastEdgeOverride[0])
+                    : bar.push(empty);
+            } else {
+                bar.push(empty);
+            }
+        }
+    }
+
+    if (returnAr == true) {
+        return bar;
     } else {
-      empty = emptyChars, filled = fullChars
+        return bar.join("");
     }
-    
-    if ( i < units) { // push the filled char
-      if (typeof firstEdgeOverride !== "undefined" && typeof lastEdgeOverride !== "undefined") {
-        i == 0 ? bar.push(firstEdgeOverride[1]) : i == barWidth - 1 ? bar.push(lastEdgeOverride[1]) : bar.push(filled)
-      } else {
-        bar.push(filled)
-      }
-    } else { // push the empty char
-      if (typeof firstEdgeOverride !== "undefined" && typeof lastEdgeOverride !== "undefined") {
-        i == 0 ? bar.push(firstEdgeOverride[0]) : i == barWidth - 1 ? bar.push(lastEdgeOverride[0]) : bar.push(empty)
-      } else {
-        bar.push(empty)
-      }
-    }
-  }
+};
 
-  if (returnAr == true) {
-    return bar
-  } else {
-    return bar.join("")
-  }
-
-}
-
-module.exports = { progressBar }
+module.exports = { progressBar };
